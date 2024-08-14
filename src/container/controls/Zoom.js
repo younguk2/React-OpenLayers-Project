008694a6111103cfc4807a1c80c21864acfa1a12
younguk2 <neostory1515@gmail.com>
2024-08-14 15:37:31 +0900

    css로 꾸미기

--- src/container/controls/Zoom.js
+++ src/container/controls/Zoom.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMap } from '../map/MapContext';
 import '../../styles/Zoom.css';
 
 export default function Zoom() {
 	const { map } = useMap(); // map element
+	const [isVisible, setIsVisible] = useState(false);
 
+	const toggleZoomContainer = () => {
+		setIsVisible(!isVisible);
+	};
 	const onZoomIn = () => {
 		let zoomLvl = map.getView().getZoom();
 		map.getView().setZoom(++zoomLvl);
@@ -16,13 +20,23 @@ export default function Zoom() {
 	};
 
 	return (
-		<div className='zoom-container'>
-			<button className='zoom-in-btn' onClick={onZoomIn}>
-				+
+		<>
+			<div
+				className='zoom-container'
+				style={{
+					right: isVisible ? '0px' : '-100px',
+				}}
+			>
+				<button className='zoom-in-btn' onClick={onZoomIn}>
+					+
+				</button>
+				<button className='zoom-out-btn' onClick={onZoomOut}>
+					-
+				</button>
+			</div>
+			<button className='toggle-btn' onClick={toggleZoomContainer}>
+				Zoom Controls
 			</button>
-			<button className='zoom-out-btn' onClick={onZoomOut}>
-				-
-			</button>
-		</div>
+		</>
 	);
 }
